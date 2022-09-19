dbf8e3513eb9f8a8896798c181d5bfdeb75293d2
JPEdgar <sevisin@hotmail.com>
2022-09-19 12:27:21 -0700

    API tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const api =
+    "https://www.googleapis.com/books/v1/volumes?q=harry+potter+inauthor:rowling";
+    // "https://www.googleapis.com/books/v1/volumes?q=harry+potter";
 
-export default App
+  const [tempRes, setTestRes] = useState();
+
+  useEffect(() => {
+    const Test = async () => {
+      const temp = await axios.get(api).then((res) => res.data);
+      console.log(temp);
+    //   console.log(JSON.parse(temp));
+    };
+
+    Test();
+  }, []);
+
+  return <div>App</div>;
+};
+
+export default App;
