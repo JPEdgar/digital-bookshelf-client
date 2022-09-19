48aab66804cceae63fa9a80eaad14daf09d168cf
JPEdgar <sevisin@hotmail.com>
2022-09-19 13:00:03 -0700

    Multiple authors added

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const [tempRes, setTestRes] = useState();
-  const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("Harry Potter");
 
   const handleChange = (e) => {
     setSearchText(e.target.value);
@@ -56,6 +56,7 @@ const App = () => {
             type="text"
             placeholder="Enter book title"
             onChange={handleChange}
+            value={searchText}
           />
         </Form.Group>
         <Button className="mt-1" onClick={() => handleClick()}>
@@ -73,21 +74,47 @@ const App = () => {
 
 const SearchResult = ({ res }) => {
   console.log(res);
-  const {volumeInfo} = res
+  const { volumeInfo } = res;
   return (
     <Card>
       <Row>
         <Col xs={4}>
-          <Image src={volumeInfo.imageLinks.thumbnail} alt={`${volumeInfo.title} cover`} />
+          <Image
+            src={volumeInfo.imageLinks.thumbnail}
+            alt={`${volumeInfo.title} cover`}
+          />
         </Col>
         <Col xs={8}>
-        <Row>
-            {volumeInfo.title}
-        </Row>
+          <div>{volumeInfo.title}</div>
+          <div>
+            by:
+            <span className="m-1">
+              {volumeInfo.authors.map((author, index) => {
+                return (
+                  <Author
+                    key={`author-${index}`}
+                    author={author}
+                    index={index}
+                    length={volumeInfo.authors.length}
+                  />
+                );
+              })}
+            </span>
+          </div>
         </Col>
       </Row>
     </Card>
   );
+};
+
+const Author = ({ author, index, length }) => {
+  let returnValue = "";
+
+  if (index === 0) returnValue = author;
+  else if (index + 1 === length) returnValue = `, and ${author}`;
+  else if (index > 0 && index + 1 !== length) returnValue = `, ${author}`;
+
+  return <span>{returnValue}</span>;
 };
 
 export default App;
