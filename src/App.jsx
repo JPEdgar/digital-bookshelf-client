3fa2aa38adc00bb0bd8578838056fc11cb721ea9
JPEdgar <sevisin@hotmail.com>
2022-09-20 15:03:50 -0700

    Design change, archived current design

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 
-import Navbar from "./Components/Navbar";
-import Search from "./Components/Search";
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <Container>
-      <Navbar />
-      <hr />
-      <Search />
+
     </Container>
   );
 };
--- src/Context/Bookshelf/BookshelfContext.jsx
+++ src/Context/Bookshelf/BookshelfContext.jsx
@@ -6,12 +6,27 @@ const useBookshelfContext = () => {
   return useContext(BookshelfContext);
 };
 
-const createBookItem = (bookTitle, isbn13) => {
-  const newISBN = typeof isbn13 === "string" ? parseInt(isbn13) : isbn13;
+const createBookItem = (bookTitle, isbnObj = {}) => {
+  let newISBN;
+
+  if (isbnObj.isbn13)
+    newISBN =
+      typeof isbn13 === "string" ? parseInt(isbnObj.isbn13) : isbnObj.isbn13;
+  else if (isbnObj.isbn10)
+    newISBN =
+      typeof isbn10 === "string" ? parseInt(isbnObj.isbn10) : isbnObj.isbn10;
+  else {
+    window.alert(
+      `Invalid ISBN for ${bookTitle} - ISBN10: ${isbnObj.isbn10} -- ISBN13: ${isbnObj.isbn13}`
+    );
+    return "error";
+  }
+
   const newBookObject = {
     id: newISBN,
     title: bookTitle,
     isbn13: newISBN,
+    // subtitle: "", isbn10: 0, inBookshelfFlag: true, onWishlistFlag: false, isSharedFlag: false, availablePrint: { hardBoundFlag: false, softBoundFlag: false }, availableDigitalFlag: false, availableAudioFlag: false,
   };
   return newBookObject;
 };
@@ -23,9 +38,9 @@ const INITIALIZE_SEARCH = {
 const INITIALIZE_BOOKSHELF = [
   createBookItem(
     "Bloodlines of Atmos - the Story of Jace - Book 3 Redemption",
-    9798886530247
+    { isbn13: 9798886530247 }
   ),
-  createBookItem("Bloodlines of Atmos", 9781680469035),
+  createBookItem("Bloodlines of Atmos", { isbn13: 9781680469035 }),
   // createBookItem("Bloodlines of Atmos", 9781680468779),
 ];
 
