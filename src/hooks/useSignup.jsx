2c2af286b9163710930d47288380fe8afa65b855
JPEdgar <sevisin@hotmail.com>
2023-01-22 00:13:00 -0800

    Cleaned file

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -22,65 +22,67 @@ const useSignup = () => {
 
     const createUserDetails = (data) => {
       const { email, id, token } = data;
-      const newUserDetails = axios.post( "http://localhost:4000/api/user", { email, id }, { headers: { Authorization: `bearer ${token}` } } );
+      const newUserDetails = axios.post(
+        "http://localhost:4000/api/user",
+        { email, id },
+        { headers: { Authorization: `bearer ${token}` } }
+      );
       return newUserDetails;
     };
 
-
+    const createBookshelf = (data) => {
+      const { email, id, token } = data;
+      const newBookshelf = axios.post(
+        "http://localhost:4000/api/bookshelf",
+        { email, id },
+        { headers: { Authorization: `bearer ${token}` } }
+      );
+      return newBookshelf;
+    };
 
     const createNewUserResponse = await signUp(email, password);
     if (createNewUserResponse.statusText !== "Created") {
-      console.log("Problem creating new user: ", createNewUserResponse);
       setLoadingFlag(false);
       setError(createNewUserResponse.error);
       return;
     }
 
-    const createNewUserDetailsResponse = await createUserDetails( createNewUserResponse.data );
+    const createNewUserDetailsResponse = await createUserDetails(
+      createNewUserResponse.data
+    );
     if (createNewUserDetailsResponse.statusText !== "Created") {
-      console.log( "Problem creating new user details: ", createNewUserDetailsResponse );
+      console.log(
+        "Problem creating new user details: ",
+        createNewUserDetailsResponse
+      );
       setLoadingFlag(false);
       setError(createNewUserDetailsResponse.error);
       return;
     }
 
-
-
-
-
-
-
-    const createBookshelf = (data) => {
-      console.log("creating new bookshelf for ", data)
-      const { email, id, token } = data;
-      const newBookshelf = axios.post( "http://localhost:4000/api/bookshelf", { email, id }, { headers: { Authorization: `bearer ${token}` } } );
-      return newBookshelf;
-    };
-
-
-
-    const createNewBookshelfResponse = await createBookshelf( createNewUserResponse.data );
-    console.log("createNewBookshelfResponse = ", createNewBookshelfResponse)
+    const createNewBookshelfResponse = await createBookshelf(
+      createNewUserResponse.data
+    );
+    console.log("createNewBookshelfResponse = ", createNewBookshelfResponse);
     if (createNewBookshelfResponse.statusText !== "Created") {
-      console.log( "Problem creating new bookshelf: ", createNewBookshelfResponse );
+      console.log(
+        "Problem creating new bookshelf: ",
+        createNewBookshelfResponse
+      );
       setLoadingFlag(false);
       setError(createNewBookshelfResponse.error);
       return;
     }
 
-
-
-
-
-
-
-
-
-
-
     // console.log("useSignup data = ", createNewUserResponse.data);
-    localStorage.setItem( "digital-bookshelf-user", JSON.stringify(createNewUserResponse.data) );
-    authDispatch({ type: AUTH_TYPES.SIGNUP, payload: createNewUserResponse.data, });
+    localStorage.setItem(
+      "digital-bookshelf-user",
+      JSON.stringify(createNewUserResponse.data)
+    );
+    authDispatch({
+      type: AUTH_TYPES.SIGNUP,
+      payload: createNewUserResponse.data,
+    });
     setUserDetails(createNewUserDetailsResponse.data);
     setLoadingFlag(false);
   };
