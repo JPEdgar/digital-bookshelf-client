37d440ecd034fbe05c642f24eace1dba80d71ede
JPEdgar <Jon.P.Edgar@gmail.com>
2023-11-22 22:18:23 -0800

    Added icons to book details

--- src/Components/BookDetails/Details.jsx
+++ src/Components/BookDetails/Details.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { Card, Stack, Image, Row, Col } from "react-bootstrap";
 
+import FavoritesIcon from "../elements/FavoritesIcon";
+import WishListIcon from "../elements/WishListIcon";
+import WantToReadIcon from "../elements/WantToReadIcon";
+import HaveReadIcon from "../elements/HaveReadIcon";
+import TrashIcon from "../elements/TrashIcon";
 import { useBookshelf } from "../../hooks";
 
 const Details = () => {
-  const { bookshelf, toggleFavorite, toggleOnBookshelf } = useBookshelf();
+  const { bookshelf, toggleFavorite, toggleOnBookshelf, isOnBookshelf } =
+    useBookshelf();
   const { bookFocus } = bookshelf || null;
-  // console.log("bookFocus = ", bookFocus);
 
   const coverImage = bookFocus?.coversList.large
     ? bookFocus.coversList.large
@@ -24,9 +29,14 @@ const Details = () => {
     bookFocus && (
       <>
         <button onClick={() => toggleOnShelf()}>Toggle onShelf</button>
-        <Stack direction="horizontal" gap={3}>
+        <Stack
+          direction="horizontal"
+          gap={3}
+          style={{ backgroundColor: "", width: "100%", height: "100%" }}
+          className="align-items-start"
+        >
           <Image src={coverImage} alt={`${bookFocus.title} cover`} thumbnail />
-          <Row>
+          <div style={{ backgroundColor: "" }} className="w-100 h-100">
             {bookFocus.title && <h4>{bookFocus.title}</h4>}
             {bookFocus.subtitle && <h5>{bookFocus.subtitle}</h5>}
             {bookFocus.authorString && <div>By: {bookFocus.authorString}</div>}
@@ -50,7 +60,16 @@ const Details = () => {
                 </Col>
               )}
             </Row>
-          </Row>
+          </div>
+          <div className="d-flex flex-column gap-2">
+            <FavoritesIcon bookData={bookFocus} />
+            <WishListIcon bookData={bookFocus} />
+            <WantToReadIcon bookData={bookFocus} />
+            <HaveReadIcon bookData={bookFocus} />
+            {isOnBookshelf(bookFocus.isbn) && (
+              <TrashIcon bookData={bookFocus} />
+            )}
+          </div>
         </Stack>
         <div>{bookFocus.snippet}</div>
       </>
