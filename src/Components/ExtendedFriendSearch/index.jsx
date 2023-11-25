32dcfb1756f1fd771632a448ac06dbb54d2d64c4
JPEdgar <Jon.P.Edgar@gmail.com>
2023-11-25 00:14:52 -0800

    Added bulk friend search cards

--- src/Components/ExtendedFriendSearch/index.jsx
+++ src/Components/ExtendedFriendSearch/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {Row} from "react-bootstrap"
 
+import FriendCard from "./FriendCard";
 import { useSearchDetails } from "../../hooks";
 
 const ExtendedFriendSearch = () => {
   const { userSearchList } = useSearchDetails();
 
-  return <div>ExtendedFriendSearch</div>;
+  return <Row className="">
+  {userSearchList.map((friendInfo) => {
+    return <FriendCard key={`friend-card-${friendInfo.userID}`} friendInfo={friendInfo}/>
+  })}
+  </Row>
 };
 
 export default ExtendedFriendSearch;
