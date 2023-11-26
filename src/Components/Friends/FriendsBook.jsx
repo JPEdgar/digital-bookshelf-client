cf11791ce5a2619f24582301a25bd7aa459ac1da
JPEdgar <Jon.P.Edgar@gmail.com>
2023-11-25 19:29:59 -0800

    Setting bookshelf links for focus

--- src/Components/Friends/FriendsBook.jsx
+++ src/Components/Friends/FriendsBook.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import {Card} from "react-bootstrap"
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const FriendsBook = ({book}) => {
-    // console.log(book)
-    const bookCover = book.coversList?.large ? book.coversList.large : book.coversList?.medium ? book.coversList.medium : book.coversList?.small
+import { cropString } from "../../utilities";
+import { useBookshelf } from "../../hooks";
+
+const FriendsBook = ({ book }) => {
+  const { setBookFocus } = useBookshelf();
+
+  const bookCover = book.coversList?.large
+    ? book.coversList.large
+    : book.coversList?.medium
+    ? book.coversList.medium
+    : book.coversList?.small;
 
   return (
-    <Card style={{ maxWidth: '10rem' }}>
-    <Card.Img variant="top" src={bookCover} />
-    <Card.Body>
-      <Card.Title>{book.title}</Card.Title>
-      {book.subtitle && <Card.Title>{book.subtitle}</Card.Title>}
-      <Card.Text>
-        {book.snippet}
-      </Card.Text>
-      
-    </Card.Body>
-  </Card>
-  )
-}
+    <Card
+      style={{ maxWidth: "10rem" }}
+      onClick={() => setBookFocus(book)}
+      as={Link}
+      to="/book-details"
+    >
+      <Card.Img variant="top" src={bookCover} />
+      <Card.Body>
+        <Card.Title>{book.title}</Card.Title>
+        {book.subtitle && <Card.Title>{book.subtitle}</Card.Title>}
+        <Card.Text>{cropString(book.snippet, 100)}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default FriendsBook
+export default FriendsBook;
