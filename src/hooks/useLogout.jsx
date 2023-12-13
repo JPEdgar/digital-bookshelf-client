3128caaf9c24b6c6160466bee23cb5d57e9c73eb
JPEdgar <Jon.P.Edgar@gmail.com>
2023-12-12 18:08:50 -0800

    Clears bookshelf when logging out

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,17 +1,21 @@
 import useAuthContext from "./context/useAuthContext"; // refactor to useAuth hook aaaaaaaaaa
 import useUserContext from "./context/useUserContext"; // refactor to useUser hook aaaaaaaaaa
+import useBookshelfContext from "./context/useBookshelfContext"; // refactor to useBookshelf hook aaaaaaaaaa
 
 import AUTH_TYPES from "../constants/types/authTypes";
 import USER_TYPES from "../constants/types/userTypes";
+import SHELF_TYPES from "../constants/types/bookshelfTypes";
 
 const useLogout = () => {
   const { dispatch: authDispatch } = useAuthContext();
   const { dispatch: userDispatch } = useUserContext();
+  const { dispatch: bookshelfDispatch } = useBookshelfContext();
 
   const logout = () => {
     localStorage.removeItem("digital-bookshelf-user");
     authDispatch({ type: AUTH_TYPES.LOGOUT });
     userDispatch({ type: USER_TYPES.LOGOUT });
+    bookshelfDispatch({ type: SHELF_TYPES.LOGOUT });
   };
 
   return { logout };
